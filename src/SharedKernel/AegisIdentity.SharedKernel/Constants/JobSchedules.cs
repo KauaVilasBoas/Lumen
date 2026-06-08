6a54aeeb375a72e19d103b470837798449354b23
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2026-06-07 22:17:06 -0300

    feat(shared): add audit event kinds, Audit.Read permission and cleanup job name constants

--- src/SharedKernel/AegisIdentity.SharedKernel/Constants/JobSchedules.cs
+++ src/SharedKernel/AegisIdentity.SharedKernel/Constants/JobSchedules.cs
@@ -2,5 +2,6 @@ namespace AegisIdentity.SharedKernel.Constants;
 
 public static class JobSchedules
 {
-    public const string DailyAt3Am = "0 3 * * *";
+    public const string DailyAt3Am    = "0 3 * * *";
+    public const string CleanupJobName = "cleanup-expired-refresh-tokens";
 }
--- src/SharedKernel/AegisIdentity.SharedKernel/Constants/Permissions.cs
+++ src/SharedKernel/AegisIdentity.SharedKernel/Constants/Permissions.cs
@@ -12,10 +12,16 @@ public static class PermissionCodes
         public const string List = "Users.List";
         public const string Get  = "Users.Get";
     }
+
+    public static class Audit
+    {
+        public const string Read = "Audit.Read";
+    }
 }
 
 public static class PermissionGroups
 {
     public const string Authorization = "Authorization";
     public const string Users         = "Users";
+    public const string Audit         = "Audit";
 }
