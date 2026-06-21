60a81230618cc7cc775b48ded1b255a60f3f32af
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2026-06-21 11:30:18 -0300

    fix(api): corrige namespace das Razor Pages scaffold de AegisIdentity.Backoffice.Pages para Lumen.Api.Pages
    As Pages em Lumen.Api/Pages tinham namespace errado herdado do template
    do scaffold, causando CS0246 no build. Atualizados namespace, using e
    titulos no _Layout.cshtml e _ViewImports.cshtml.

--- src/Lumen.Api/Pages/Privacy.cshtml.cs
+++ src/Lumen.Api/Pages/Privacy.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Lumen.Backoffice.Pages;
+namespace Lumen.Api.Pages;
 
 [AllowAnonymous]
 public class PrivacyModel : PageModel
