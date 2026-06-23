2f34ba7529f3b1410c0ab645e59329888755e414
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2026-06-22 23:51:02 -0300

    refactor(domain): migra DuplicateEmailException para herdar de ConflictException

--- src/Lumen.Domain/Users/DuplicateEmailException.cs
+++ src/Lumen.Domain/Users/DuplicateEmailException.cs
@@ -1,6 +1,8 @@
+using Lumen.SharedKernel.Exceptions;
+
 namespace Lumen.Domain.Users;
 
-public sealed class DuplicateEmailException : Exception
+public sealed class DuplicateEmailException : ConflictException
 {
     public DuplicateEmailException(string email)
         : base($"A user with email '{email}' already exists.")
