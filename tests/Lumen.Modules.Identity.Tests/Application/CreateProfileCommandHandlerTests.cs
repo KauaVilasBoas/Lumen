db686dfac3322b2cf3eb32ed7a0cef1d3b28875e
Kauã Vilas Boas <kauacaldeira@hotmail.com>
2026-06-30 00:20:06 -0300

    test(identity): add handler and validator tests for Profile and UserProfile management flows

---
+++ tests/Lumen.Modules.Identity.Tests/Application/CreateProfileCommandHandlerTests.cs
@@ -0,0 +1,84 @@
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Lumen.Modules.Identity.Application.Profiles.Create;
+using Lumen.Modules.Identity.Domain.Authorization;
+using Lumen.SharedKernel.Exceptions;
+using NSubstitute;
+
+namespace Lumen.Modules.Identity.Tests.Application;
+
+public sealed class CreateProfileCommandHandlerTests
+{
+    private readonly IProfileRepository _profileRepository = Substitute.For<IProfileRepository>();
+
+    private CreateProfileCommandHandler CreateHandler()
+        => new(_profileRepository);
+
+    [Fact]
+    public async Task Handle_UniqueName_CreatesProfileAndReturnsResult()
+    {
+        _profileRepository
+            .ActiveNameExistsAsync("Managers", null, Arg.Any<CancellationToken>())
+            .Returns(false);
+
+        var handler = CreateHandler();
+        var result = await handler.Handle(
+            new CreateProfileCommand("Managers", "Manages resources"),
+            CancellationToken.None);
+
+        result.Name.Should().Be("Managers");
+        result.Description.Should().Be("Manages resources");
+        result.Id.Should().NotBeEmpty();
+
+        await _profileRepository.Received(1).InsertAsync(Arg.Any<Profile>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task Handle_DuplicateName_ThrowsConflictException()
+    {
+        _profileRepository
+            .ActiveNameExistsAsync("Managers", null, Arg.Any<CancellationToken>())
+            .Returns(true);
+
+        var handler = CreateHandler();
+        var act = async () => await handler.Handle(
+            new CreateProfileCommand("Managers", "Desc"),
+            CancellationToken.None);
+
+        await act.Should().ThrowAsync<ConflictException>();
+        await _profileRepository.DidNotReceive().InsertAsync(Arg.Any<Profile>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public void Validator_EmptyName_ProducesError()
+    {
+        var validator = new CreateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new CreateProfileCommand("", "Desc"));
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void Validator_NameTooLong_ProducesError()
+    {
+        var longName = new string('a', 129);
+        var validator = new CreateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new CreateProfileCommand(longName, "Desc"));
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void Validator_EmptyDescription_ProducesError()
+    {
+        var validator = new CreateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new CreateProfileCommand("Name", ""));
+        result.ShouldHaveValidationErrorFor(x => x.Description);
+    }
+
+    [Fact]
+    public void Validator_ValidCommand_HasNoErrors()
+    {
+        var validator = new CreateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new CreateProfileCommand("Managers", "Manages resources"));
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
---
+++ tests/Lumen.Modules.Identity.Tests/Application/UpdateProfileCommandHandlerTests.cs
@@ -0,0 +1,131 @@
+using FluentAssertions;
+using FluentValidation.TestHelper;
+using Lumen.Modules.Identity.Application.Profiles.Update;
+using Lumen.Modules.Identity.Domain.Authorization;
+using Lumen.SharedKernel.Exceptions;
+using NSubstitute;
+
+namespace Lumen.Modules.Identity.Tests.Application;
+
+public sealed class UpdateProfileCommandHandlerTests
+{
+    private readonly IProfileRepository _profileRepository = Substitute.For<IProfileRepository>();
+
+    private UpdateProfileCommandHandler CreateHandler()
+        => new(_profileRepository);
+
+    [Fact]
+    public async Task Handle_ValidUpdate_UpdatesProfile()
+    {
+        var profileId = Guid.NewGuid();
+        var profile = Profile.Create("OldName", "Old description");
+
+        _profileRepository.FindByIdAsync(profileId, Arg.Any<CancellationToken>()).Returns(profile);
+        _profileRepository
+            .ActiveNameExistsAsync("NewName", profileId, Arg.Any<CancellationToken>())
+            .Returns(false);
+
+        var handler = CreateHandler();
+        await handler.Handle(
+            new UpdateProfileCommand(profileId, "NewName", "New description"),
+            CancellationToken.None);
+
+        await _profileRepository.Received(1).UpdateAsync(
+            Arg.Is<Profile>(p => p.Name == "NewName" && p.Description == "New description"),
+            Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task Handle_ProfileNotFound_ThrowsNotFoundException()
+    {
+        _profileRepository
+            .FindByIdAsync(Arg.Any<Guid>(), Arg.Any<CancellationToken>())
+            .Returns((Profile?)null);
+
+        var handler = CreateHandler();
+        var act = async () => await handler.Handle(
+            new UpdateProfileCommand(Guid.NewGuid(), "Name", "Desc"),
+            CancellationToken.None);
+
+        await act.Should().ThrowAsync<NotFoundException>();
+    }
+
+    [Fact]
+    public async Task Handle_SystemProfileRename_ThrowsForbiddenException()
+    {
+        var profileId = Guid.NewGuid();
+        var systemProfile = Profile.Create("Administrator", "System managed", isSystem: true);
+
+        _profileRepository.FindByIdAsync(profileId, Arg.Any<CancellationToken>()).Returns(systemProfile);
+
+        var handler = CreateHandler();
+        var act = async () => await handler.Handle(
+            new UpdateProfileCommand(profileId, "NewName", "Desc"),
+            CancellationToken.None);
+
+        await act.Should().ThrowAsync<ForbiddenException>();
+    }
+
+    [Fact]
+    public async Task Handle_SystemProfileDescriptionUpdate_Succeeds()
+    {
+        var profileId = Guid.NewGuid();
+        var systemProfile = Profile.Create("Administrator", "Old description", isSystem: true);
+
+        _profileRepository.FindByIdAsync(profileId, Arg.Any<CancellationToken>()).Returns(systemProfile);
+        _profileRepository
+            .ActiveNameExistsAsync("Administrator", profileId, Arg.Any<CancellationToken>())
+            .Returns(false);
+
+        var handler = CreateHandler();
+        await handler.Handle(
+            new UpdateProfileCommand(profileId, "Administrator", "New description"),
+            CancellationToken.None);
+
+        await _profileRepository.Received(1).UpdateAsync(Arg.Any<Profile>(), Arg.Any<CancellationToken>());
+    }
+
+    [Fact]
+    public async Task Handle_DuplicateName_ThrowsConflictException()
+    {
+        var profileId = Guid.NewGuid();
+        var profile = Profile.Create("OldName", "Old description");
+
+        _profileRepository.FindByIdAsync(profileId, Arg.Any<CancellationToken>()).Returns(profile);
+        _profileRepository
+            .ActiveNameExistsAsync("TakenName", profileId, Arg.Any<CancellationToken>())
+            .Returns(true);
+
+        var handler = CreateHandler();
+        var act = async () => await handler.Handle(
+            new UpdateProfileCommand(profileId, "TakenName", "Desc"),
+            CancellationToken.None);
+
+        await act.Should().ThrowAsync<ConflictException>();
+    }
+
+    [Fact]
+    public void Validator_EmptyId_ProducesError()
+    {
+        var validator = new UpdateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new UpdateProfileCommand(Guid.Empty, "Name", "Desc"));
+        result.ShouldHaveValidationErrorFor(x => x.Id);
+    }
+
+    [Fact]
+    public void Validator_EmptyName_ProducesError()
+    {
+        var validator = new UpdateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(new UpdateProfileCommand(Guid.NewGuid(), "", "Desc"));
+        result.ShouldHaveValidationErrorFor(x => x.Name);
+    }
+
+    [Fact]
+    public void Validator_ValidCommand_HasNoErrors()
+    {
+        var validator = new UpdateProfileCommandHandler.Validator();
+        var result = validator.TestValidate(
+            new UpdateProfileCommand(Guid.NewGuid(), "Managers", "Manages resources"));
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
